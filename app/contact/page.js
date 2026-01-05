6d6046c58dc6edff96cfaf24e08ace030f8c0fc4
rdgtech-ctrl <dg.mechpro200@gmail.com>
2026-01-05 23:43:22 +0530

    Update files

--- app/contact/page.js
+++ app/contact/page.js
@@ -8,7 +8,7 @@ const contact = () => {
         <div>
             <div className='flex flex-row justify-between rounded-2xl items-center bg-purple-100 min-h-screen  md:m-4 p-4 md:p-7 gap-6 md:gap-8 border-4 md:border-6 border-purple-400'>
 
-                <div className="h-48 w-48 sm:h-64 sm:w-64 md:h-80 md:w-80 lg:h-96 lg:w-96 flex justify-center relative">
+                <div className="h-48 w-28 sm:h-64 sm:w-64 md:h-80 md:w-80 lg:h-96 lg:w-96 flex justify-center relative">
                     <Image
                         className="mix-blend-darken object-contain"
                         src={"/contactus.jpg"}
