decdd877f1c93a8d0a5c8f231e74ee054e04e0c2
rdgtech-ctrl <dg.mechpro200@gmail.com>
2026-01-06 01:32:15 +0530

    Update files

--- app/shorten/page.js
+++ app/shorten/page.js
@@ -4,7 +4,33 @@ import React, { useState } from 'react'
 const Shorten = () => {
     const [url, seturl] = useState("")
     const [shorturl, setshorturl] = useState("")
-    const [generate, setGenerated] = useState(false)
+    const [isGenerated, setGenerated] = useState(false)
+
+    //This is a fetch request
+    const generate = () => {
+        const myHeaders = new Headers();
+        myHeaders.append("Content-Type", "application/json");
+
+        const raw = JSON.stringify({
+            "url": url,
+            "shorturl": shorturl
+        });
+
+        const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body: raw,
+            redirect: "follow"
+        };
+
+        fetch("/api/generate", requestOptions)
+            .then((response) => response.json())
+            .then((result) => {
+                console.log(result)
+                alert(result.message)
+            })
+            .catch((error) => console.error(error));
+    }
 
     return (
         <div className='mx-auto max-w-lg bg-purple-100 my-16 p-8 rounded-lg flex flex-col gap-4'>
@@ -17,7 +43,7 @@ const Shorten = () => {
                     className='px-4 py-2  focus:outline-purple-600 rounded-md'
                     placeholder="Enter your preferred short URL text"
                     onChange={e => { setshorturl(e.target.value) }} />
-                <button className='bg-purple-900 rounded-lg shadow-lg p-3 py-1 my-3 font-bold text-white'>Generate</button>
+                <button onClick={generate} className='bg-purple-900 rounded-lg shadow-lg p-3 py-1 my-3 font-bold text-white'>Generate</button>
             </div>
         </div>
     );
